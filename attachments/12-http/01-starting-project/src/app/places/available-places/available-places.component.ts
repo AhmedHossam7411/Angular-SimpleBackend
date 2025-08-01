40f95bb140cd0e1e3e18c27a89195daa762c40da
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-08-01 21:02:35 +0300

    retrieving pictures from backend

--- attachments/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ attachments/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -20,14 +20,14 @@ export class AvailablePlacesComponent {
   private destroyRef = inject(DestroyRef); // Injecting DestroyRef to manage component lifecycle
   ngOnInit() // Lifecycle hook to fetch places when the component initializes
   {
-    const subscription = this.httpClient.get<{places:Place[]}>('http://localhost:3000/places',
-    {
-      observe: 'response',  // Observing the full HTTP response
-
-    }).subscribe({
-      next: (response) => {
-        console.log(response)
-        console.log(response.body?.places);  // Logging the places received from the server
+    const subscription = this.httpClient.get<{places:Place[]}>('http://localhost:3000/places')
+    .pipe(
+      map((resData) => resData.places)
+      )
+      .subscribe({
+      next: (places) => {
+        
+        this.places.set(places)  // Logging the places received from the server
       }
     });
       
