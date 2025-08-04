54db352a602c558bbeed8803b0e38167b8216d59
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-08-04 10:51:48 +0300

    error handling for failed fetching

--- attachments/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ attachments/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map, Subscription } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -15,20 +15,36 @@ import { map, Subscription } from 'rxjs';
 })
 export class AvailablePlacesComponent {
   places = signal<Place[] | undefined>(undefined);
-
+  isFetching = signal(false); // Signal to track fetching state
+  error = signal(''); // Signal to track any error that occurs
   private httpClient= inject(HttpClient);  // Injecting HttpClient for making HTTP requests
   private destroyRef = inject(DestroyRef); // Injecting DestroyRef to manage component lifecycle
+
   ngOnInit() // Lifecycle hook to fetch places when the component initializes
   {
+    this.isFetching.set(true); // Setting fetching state to true before making the request
     const subscription = this.httpClient.get<{places:Place[]}>('http://localhost:3000/places')
-    .pipe(
-      map((resData) => resData.places)
+    .pipe(  // Using RxJS pipe to transform the observable 
+      map((resData) => resData.places),catchError  
+      ((error) => 
+        throwError(()=> new Error(''))) // Handling errors and transforming the response
+      // Mapping the response to extract the places array
       )
-      .subscribe({
-      next: (places) => {
+      .subscribe({  // Subscribing to the observable to get the places data
+      next: (places) => {  // Handling the next value emitted by the observable
         
         this.places.set(places)  // Logging the places received from the server
-      }
+      },
+
+      error: (error) => {
+        console.log(error); 
+        this.error.set("something went wrong while fetching placesf");
+         // Setting error message if an error occurs
+      
+        },  // Handling any error that occurs during the request
+      complete: () => {  // Handling completion of the observable
+        this.isFetching.set(false); // Setting fetching state to false after the request completes because the data is fetched
+      }, 
     });
       
     this.destroyRef.onDestroy(() => {  // Cleanup logic when the component is destroyed
