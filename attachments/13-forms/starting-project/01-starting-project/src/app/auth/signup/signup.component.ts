0385e4f60849307e02688557b950a7f8867ee657
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-08-06 13:19:34 +0300

    connecting all form fields to reactive formcontrol

--- attachments/13-forms/starting-project/01-starting-project/src/app/auth/signup/signup.component.ts
+++ attachments/13-forms/starting-project/01-starting-project/src/app/auth/signup/signup.component.ts
@@ -16,7 +16,7 @@ export class SignupComponent
   {
     console.log();
   }
-  
+
   onReset(){
   this.form.reset();
 }
@@ -28,6 +28,20 @@ export class SignupComponent
          password: new FormControl('',{
            validators: [Validators.required, Validators.minLength(6),mustContainQuestionMark],
           }),
+          confirmPassword: new FormControl('',{
+           validators: [Validators.required, Validators.minLength(6),mustContainQuestionMark],
+          }),
+          firstName: new FormControl('',{ validators: [Validators.required],}),
+          lastName: new FormControl('',{ validators: [Validators.required],}),
+          street: new FormControl('',{ validators: [Validators.required],}),
+          number: new FormControl('',{ validators: [Validators.required],}),
+          postalCode: new FormControl('',{ validators: [Validators.required],}),
+          city: new FormControl('',{ validators: [Validators.required],}),
+          
+          role : new FormControl<
+          'student' | 'teacher' | 'employee' | 'founder' | 'other'
+          >('student', {validators : [Validators.required]}),
+          agree : new FormControl(false,{ validators: [Validators.required]})
        });
-   
+      
 }
